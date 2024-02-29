2ac5bbcec44d831a1008ee3d279b6d04c57ec34b
Volodymyr <terces@ukr.net>
2024-02-29 11:00:16 +0200

    fix

--- quest1/light.py
+++ quest1/light.py
@@ -12,3 +12,14 @@
 # print(is_isogram("aba"))  # True
 
 
+def is_isogram(string):
+    string = string.lower()
+    for char in string:
+        if string.count(char) > 1:
+            return False
+        return True
+
+
+
+
+print(is_isogram("aba"))
